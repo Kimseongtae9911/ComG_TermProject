bf14875e872711882b3aaf373b744171b0335423
chd4433 <chdkhj4433@naver.com>
2021-11-20 20:08:50 +0900

    Update stdafx.h

--- TermProject/stdafx.h
+++ TermProject/stdafx.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <gl/glew.h>
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h>
+
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <list>
+#include <map>
+#include <vector>
+#include <time.h>
+#include <Windows.h>
+#include <math.h>
+#include <random>
+
+using namespace std;
+using namespace glm;

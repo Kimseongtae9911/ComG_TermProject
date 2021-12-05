19431518be357fb8d1e867d516b3073ddf0dbffc
chd4433 <chdkhj4433@naver.com>
2021-12-05 15:05:40 +0900

    CObj

--- TermProject/Cobj.cpp
+++ TermProject/Cobj.cpp
@@ -1,1 +1,38 @@
+#include "stdafx.h"
+#include "Manager.h"
 #include "CObj.h"
+
+CObj::CObj()
+{
+}
+
+CObj::~CObj()
+{
+}
+
+HRESULT CObj::Initialize()
+{
+	m_pGameMgr = CGameManager::GetInstance();
+	m_pRender = CRenderManager::GetInstance();
+	m_pSceneMgr = CSceneManager::GetInstance();
+	m_pKeyMgr = CKeyManager::GetInstance();
+	m_pShaderLoader = CShader::GetInstance();
+
+
+	return NOERROR;
+}
+
+GLint CObj::Update(const GLfloat fTimeDelta)
+{
+	return GLint();
+}
+
+GLvoid CObj::Render()
+{
+	return GLvoid();
+}
+
+GLvoid CObj::Release()
+{
+	return GLvoid();
+}
--- TermProject/Cobj.h
+++ TermProject/Cobj.h
@@ -1,5 +1,23 @@
 #pragma once
 class CObj
 {
+public:
+	CObj();
+	~CObj();
+
+public:
+	virtual HRESULT Initialize();
+	virtual GLint Update(const GLfloat fTimeDelta);
+	virtual GLvoid Render();
+
+protected:
+	CRenderManager* m_pRender = nullptr;
+	CKeyManager* m_pKeyMgr = nullptr;
+	CGameManager* m_pGameMgr = nullptr;
+	CSceneManager* m_pSceneMgr = nullptr;
+	CShader* m_pShaderLoader = nullptr;
+
+protected:
+	GLvoid Release();
 };
 

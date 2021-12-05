1c4f8bd99d9f9b6e89d37cb97857783236752a91
chd4433 <chdkhj4433@naver.com>
2021-12-05 19:13:03 +0900

    modify shader and load obj

--- TermProject/CShader.cpp
+++ TermProject/CShader.cpp
@@ -3,26 +3,68 @@
 
 IMPLEMENT_SINGLETON(CShader)
 
-HRESULT CShader::Initialize_Shader(string strVertex, string strFragment)
+CShader::CShader()
 {
-	if (FAILED(Create_VertexShader(strVertex)))
+}
+
+
+CShader::~CShader()
+{
+}
+
+HRESULT CShader::Add_Shader(string strTag, string strVertex, string strPixel)
+{
+	SHADER* pShader = new SHADER;
+
+	if (FAILED(Create_VertexShader(pShader, strVertex)))
+	{
+		SafeDelete(pShader);
 		return E_FAIL;
+	}
 
-	if (FAILED(Create_FragmentShader(strFragment)))
+	if (FAILED(Create_FragmentShader(pShader, strPixel)))
+	{
+		SafeDelete(pShader);
 		return E_FAIL;
+	}
 
-	s_program = glCreateProgram();
-	glAttachShader(s_program, vertexshader);
-	glAttachShader(s_program, fragmentshader);
-	glLinkProgram(s_program);
+	pShader->program = glCreateProgram();
+	glAttachShader(pShader->program, pShader->vertexshader);
+	glAttachShader(pShader->program, pShader->fragmentshader);
+	glLinkProgram(pShader->program);
 
-	glDeleteShader(vertexshader);
-	glDeleteShader(fragmentshader);
+	glDeleteShader(pShader->vertexshader);
+	glDeleteShader(pShader->fragmentshader);
 
-	glUseProgram(s_program);
+	glUseProgram(pShader->program);
+
+	m_mapShader.insert({ strTag, pShader });
 
 	return NOERROR;
 }
+
+GLuint CShader::Use_Shader(string strTag)
+{
+	auto iter_find = m_mapShader.find(strTag);
+
+	if (iter_find == m_mapShader.end())
+	{
+		m_iCurrProgram = -1;
+		cout << "ERROR : failed to find shader" << endl;
+		return E_FAIL;
+	}
+
+	m_iCurrProgram = iter_find->second->program;
+	glUseProgram(m_iCurrProgram);
+
+	return m_iCurrProgram;
+}
+
+GLuint CShader::Get_CurrProgram()
+{
+	return m_iCurrProgram;
+}
+
 
 char* CShader::FileToBuf(const char* file)
 {
@@ -42,22 +84,25 @@ char* CShader::FileToBuf(const char* file)
 	return buf; // Return the buffer
 }
 
-HRESULT CShader::Create_VertexShader(string strVertexShader)
+HRESULT CShader::Create_VertexShader(SHADER* pShader, string strVertexShader)
 {
-	vertexsource = FileToBuf(strVertexShader.c_str());
+	if (pShader == nullptr)
+		return E_FAIL;
+
+	pShader->vertexsource = FileToBuf(strVertexShader.c_str());
 	//--- 버텍스 세이더 객체 만들기
-	vertexshader = glCreateShader(GL_VERTEX_SHADER);
+	pShader->vertexshader = glCreateShader(GL_VERTEX_SHADER);
 	//--- 세이더 코드를 세이더 객체에 넣기
-	glShaderSource(vertexshader, 1, (const GLchar**)&vertexsource, 0);
+	glShaderSource(pShader->vertexshader, 1, (const GLchar**)&pShader->vertexsource, 0);
 	//--- 버텍스 세이더 컴파일하기
-	glCompileShader(vertexshader);
+	glCompileShader(pShader->vertexshader);
 	//--- 컴파일이 제대로 되지 않은 경우: 에러 체크
 	GLint result;
 	GLchar errorLog[512];
-	glGetShaderiv(vertexshader, GL_COMPILE_STATUS, &result);
+	glGetShaderiv(pShader->vertexshader, GL_COMPILE_STATUS, &result);
 	if (!result)
 	{
-		glGetShaderInfoLog(vertexshader, 512, NULL, errorLog);
+		glGetShaderInfoLog(pShader->vertexshader, 512, NULL, errorLog);
 		cerr << "ERROR : vertex shader compile failed\n" << errorLog << endl;
 		return E_FAIL;
 	}
@@ -65,22 +110,25 @@ HRESULT CShader::Create_VertexShader(string strVertexShader)
 	return NOERROR;
 }
 
-HRESULT CShader::Create_FragmentShader(string strfragmentShader)
+HRESULT CShader::Create_FragmentShader(SHADER* pShader, string strfragmentShader)
 {
-	fragmentsource = FileToBuf(strfragmentShader.c_str());
+	if (pShader == nullptr)
+		return E_FAIL;
+
+	pShader->fragmentsource = FileToBuf(strfragmentShader.c_str());
 	//--- 프래그먼트 세이더 객체 만들기
-	fragmentshader = glCreateShader(GL_FRAGMENT_SHADER);
+	pShader->fragmentshader = glCreateShader(GL_FRAGMENT_SHADER);
 	//--- 세이더 코드를 세이더 객체에 넣기
-	glShaderSource(fragmentshader, 1, (const GLchar**)&fragmentsource, 0);
+	glShaderSource(pShader->fragmentshader, 1, (const GLchar**)&pShader->fragmentsource, 0);
 	//--- 프래그먼트 세이더 컴파일
-	glCompileShader(fragmentshader);
+	glCompileShader(pShader->fragmentshader);
 	//--- 컴파일이 제대로 되지 않은 경우: 컴파일 에러 체크
 	GLint result;
 	GLchar errorLog[512];
-	glGetShaderiv(fragmentshader, GL_COMPILE_STATUS, &result);
+	glGetShaderiv(pShader->fragmentshader, GL_COMPILE_STATUS, &result);
 	if (!result)
 	{
-		glGetShaderInfoLog(fragmentshader, 512, NULL, errorLog);
+		glGetShaderInfoLog(pShader->fragmentshader, 512, NULL, errorLog);
 		cerr << "ERROR: fragment shader compile failed\n" << errorLog << endl;
 		return E_FAIL;
 	}

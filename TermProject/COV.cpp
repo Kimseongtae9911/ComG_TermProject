9f74b9c2a31df9c07c30b564cdf44e70a2f1bf66
KimSeongtae <seongtae07@gmail.com>
2021-12-11 01:37:47 +0900

    2D Resource Alpha Blending

--- TermProject/COV.cpp
+++ TermProject/COV.cpp
@@ -60,7 +60,7 @@ GLvoid drawScene(GLvoid)
 {
 	if (CFrameManager::GetInstance()->FrameLimit(60.f))
 	{
-		glClearColor(1, 1, 1, 1);
+		glClearColor(0, 0, 0, 1);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		CFrameManager::GetInstance()->UpdateTime();

d71eeefdd31a7da4d7d8120d9877178641787084
KimSeongtae <seongtae07@gmail.com>
2021-12-11 12:56:37 +0900

    making key

--- TermProject/Monster.cpp
+++ TermProject/Monster.cpp
@@ -19,6 +19,7 @@ HRESULT Monster::Initialize(glm::vec3 vPos)
 	CObj::Initialize();
 	m_pMonster = CMesh::Create("../Resource/Monster/podoboo.obj", { 1.0, 1.0, 1.0, m_fAlpha });
 	m_pMonster->GetScale() = glm::vec3(0.03, 0.03, 0.03);
+	//m_pMonster->GetParent() = 
 	m_pMonster->GetPos() = vPos;
 	return NOERROR;
 }
